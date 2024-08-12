47339b699fb15c308816ec5804901ee875176d06
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-08-12 16:00:32 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import CounterContextProvider from './store/CounterContext';
+import store from './store/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <CounterContextProvider>
+    <Provider store={store}>
       <App />
-    </CounterContextProvider>
+    </Provider>
   </React.StrictMode>
 );
