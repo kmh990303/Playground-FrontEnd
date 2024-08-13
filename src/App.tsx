12f0f0e7e9cc65394e3671007a198655ad9a134c
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-08-13 21:24:56 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,78 @@
-import { useState } from "react";
-import Com from "./component/Com";
-// import Counter from "./component/Counter";
-// import Position from "./component/Position";
-// import Time from "./component/Time";
+import React, { Suspense } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { movieLoader } from "./pages/Movie";
+// import Home from "./pages/Home";
+// import Movie from "./pages/Movie";
+// import MovieDetail from "./pages/MovieDetail";
+// import { movieLoader } from "./pages/Movie";
+// import Root from "./pages/Root";
+// import Error from "./pages/Error";
+// import Form from "./pages/FormSubmit";
 
-function App() {
-  const [num, setNum] = useState<number>(0);
+const Home = React.lazy(() => import('./pages/Home'));
+const Movie = React.lazy(() => import('./pages/Movie'));
+const MovieDetail = React.lazy(() => import('./pages/MovieDetail'));
+const Root = React.lazy(() => import("./pages/Root"));
+const Error = React.lazy(() => import('./pages/Error'));
+const FormSubmit = React.lazy(() => import('./pages/FormSubmit'));
 
-  const changeHandler = () => {
-    setNum((prevState) => {
-      return prevState + 10;
-    })
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <Root />
+      </Suspense>
+    ),
+    errorElement: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <Error />
+      </Suspense>
+    ),
+    children: [
+      {
+        path: '',
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Home />
+          </Suspense>
+        )
+      },
+      {
+        path: 'movie',
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Movie />
+          </Suspense>
+        ),
+        loader: movieLoader,
+        id: 'movie', // 부모 라우트에 ID를 설정
+        children: [
+          {
+            path: 'movieDetail',
+            element: (
+              <Suspense fallback={<div>Loading...</div>}>
+                <MovieDetail />
+              </Suspense>
+            )
+          }
+        ]
+      },
+      {
+        path: 'form',
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <FormSubmit />
+          </Suspense>
+        )
+      }
+    ]
   }
+]);
 
-  return (
-    <div>
-      <Com num={num} setNum={setNum} />
-      <button onClick={changeHandler}>Change</button>
-    </div>
-  );
+const App: React.FC = () => {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
