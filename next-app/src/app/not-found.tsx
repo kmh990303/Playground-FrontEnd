653ada1f752187650171e8e0066d02a504deab22
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-08-22 20:26:56 +0900

    Add files via upload

---
+++ next-app/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound: React.FC = () => {
+    return (
+        <div className="text-center">
+            its NOT FOUND...Please check route!!!
+        </div>
+    )
+}
+
+export default NotFound;
