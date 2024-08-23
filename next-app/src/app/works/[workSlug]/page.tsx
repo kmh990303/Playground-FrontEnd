e8a57e8c59bb5fd4c94a8c1637c459f8e9765774
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-08-23 16:36:46 +0900

    Update page.tsx

--- next-app/src/app/works/[workSlug]/page.tsx
+++ next-app/src/app/works/[workSlug]/page.tsx
@@ -3,9 +3,21 @@ import React from "react";
 
 import { getWork } from "@/lib/works";
 
-
 interface paramType {
   workSlug: string;
+}
+
+export async function generateMetadata({ params }: { params: paramType }) {
+  const work = await getWork(params.workSlug);
+
+  if (!work) {
+    notFound();
+  }
+
+  return {
+    title: work.title,
+    description: work.description,
+  };
 }
 
 const WorkDetail: React.FC<{ params: paramType }> = ({ params }) => {
@@ -15,15 +27,13 @@ const WorkDetail: React.FC<{ params: paramType }> = ({ params }) => {
     notFound();
   }
 
-  work.description = work.description.replace(/\n/g, '<br/>');
+  work.description = work.description.replace(/\n/g, "<br/>");
 
   return (
     <>
       <h1>{params.workSlug} detail page</h1>
-      <p>
-        {work.title}
-      </p>
-      <p dangerouslySetInnerHTML={{__html: work.description}}></p>
+      <p>{work.title}</p>
+      <p dangerouslySetInnerHTML={{ __html: work.description }}></p>
     </>
   );
 };
